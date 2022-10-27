eb8696ed590e266903e2bdbae5e675c4d88e8330
jovo-zmaj <116739573+jovo-zmaj@users.noreply.github.com>
2022-10-27 09:11:33 +0200

    ajmo radi

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-import pygame, sys
+import pygame, sys, math
 from pygame.locals import QUIT
 
 pygame.init()
-EKRAN = pygame.display.set_mode((400, 300))
+xE=1000
+yE=1000
+EKRAN = pygame.display.set_mode((xE, yE))
 pygame.display.set_caption('cao')
 
-def rek():
-	pass
+glu=1.5
+
+def rek(u,d,x,y):
+	if d<2: return
+	su=d*math.sin(u)
+	cu=d*math.cos(u)
+	pygame.draw.line(EKRAN,(255,255,255),(x,y),(x+su,y-cu),2)	
+	#crtaj
+	rek(u+glu,d/1.5,x+su,y-cu)
+	rek(u-glu,d/1.5,x+su,y-cu)
+	return
 
 
 def main():
-	while True:
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				sys.exit()
-			pygame.display.update()
-
+        rek(0,yE/4,xE/2,yE)
+        pygame.display.update()
+        while True:
+                for event in pygame.event.get():
+                        if event.type == QUIT:
+                                pygame.quit()
+                                sys.exit()
+			
 main()
